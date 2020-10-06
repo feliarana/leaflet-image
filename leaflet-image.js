1f702da9a4f791fe39667e337f29d6e8baf37e3b
Feli Arana <felipearana10@hotmail.com>
2020-10-06 14:41:59 -0300

    Update leaflet-image.js

--- leaflet-image.js
+++ leaflet-image.js
@@ -1,14 +1,8 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.leafletImage = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/* global L */
-
-var queue = require('d3-queue').queue;
-
-var cacheBusterDate = +new Date();
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.leafletImage=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+var queue = _dereq_('./queue');
 
 // leaflet-image
 module.exports = function leafletImage(map, callback) {
-
-    var hasMapbox = !!L.mapbox;
 
     var dimensions = map.getSize(),
         layerQueue = new queue(1);
@@ -18,45 +12,20 @@ module.exports = function leafletImage(map, callback) {
     canvas.height = dimensions.y;
     var ctx = canvas.getContext('2d');
 
-    // dummy canvas image when loadTile get 404 error
-    // and layer don't have errorTileUrl
-    var dummycanvas = document.createElement('canvas');
-    dummycanvas.width = 1;
-    dummycanvas.height = 1;
-    var dummyctx = dummycanvas.getContext('2d');
-    dummyctx.fillStyle = 'rgba(0,0,0,0)';
-    dummyctx.fillRect(0, 0, 1, 1);
-
     // layers are drawn in the same order as they are composed in the DOM:
     // tiles, paths, and then markers
     map.eachLayer(drawTileLayer);
-    map.eachLayer(drawEsriDynamicLayer);
-    
-    if (map._pathRoot) {
-        layerQueue.defer(handlePathRoot, map._pathRoot);
-    } else if (map._panes) {
-        var firstCanvas = map._panes.overlayPane.getElementsByTagName('canvas').item(0);
-        if (firstCanvas) { layerQueue.defer(handlePathRoot, firstCanvas); }
-    }
+    if (map._pathRoot) layerQueue.defer(handlePathRoot, map._pathRoot);
     map.eachLayer(drawMarkerLayer);
     layerQueue.awaitAll(layersDone);
 
     function drawTileLayer(l) {
         if (l instanceof L.TileLayer) layerQueue.defer(handleTileLayer, l);
-        else if (l._heat) layerQueue.defer(handlePathRoot, l._canvas);
     }
 
     function drawMarkerLayer(l) {
         if (l instanceof L.Marker && l.options.icon instanceof L.Icon) {
             layerQueue.defer(handleMarkerLayer, l);
-        }
-    }
-    
-    function drawEsriDynamicLayer(l) {
-        if (!L.esri) return;
-       
-        if (l instanceof L.esri.DynamicMapLayer) {                       
-            layerQueue.defer(handleEsriDymamicLayer, l);
         }
     }
 
@@ -66,7 +35,7 @@ module.exports = function leafletImage(map, callback) {
 
     function layersDone(err, layers) {
         if (err) throw err;
-        layers.forEach(function (layer) {
+        layers.forEach(function(layer) {
             if (layer && layer.canvas) {
                 ctx.drawImage(layer.canvas, 0, 0);
             }
@@ -75,31 +44,35 @@ module.exports = function leafletImage(map, callback) {
     }
 
     function handleTileLayer(layer, callback) {
-        // `L.TileLayer.Canvas` was removed in leaflet 1.0
-        var isCanvasLayer = (L.TileLayer.Canvas && layer instanceof L.TileLayer.Canvas),
-            canvas = document.createElement('canvas');
+        var canvas = document.createElement('canvas');
 
         canvas.width = dimensions.x;
         canvas.height = dimensions.y;
 
         var ctx = canvas.getContext('2d'),
             bounds = map.getPixelBounds(),
+            origin = map.getPixelOrigin(),
             zoom = map.getZoom(),
             tileSize = layer.options.tileSize;
 
         if (zoom > layer.options.maxZoom ||
             zoom < layer.options.minZoom ||
             // mapbox.tileLayer
-            (hasMapbox &&
-                layer instanceof L.mapbox.tileLayer && !layer.options.tiles)) {
+            (layer.options.format && !layer.options.tiles)) {
             return callback();
         }
+
+        var offset = new L.Point(
+            ((origin.x / tileSize) - Math.floor(origin.x / tileSize)) * tileSize,
+            ((origin.y / tileSize) - Math.floor(origin.y / tileSize)) * tileSize
+        );
 
         var tileBounds = L.bounds(
             bounds.min.divideBy(tileSize)._floor(),
             bounds.max.divideBy(tileSize)._floor()),
             tiles = [],
-            j, i,
+            center = tileBounds.getCenter(),
+            j, i, point,
             tileQueue = new queue(1);
 
         for (j = tileBounds.min.y; j <= tileBounds.max.y; j++) {
@@ -108,60 +81,34 @@ module.exports = function leafletImage(map, callback) {
             }
         }
 
-        tiles.forEach(function (tilePoint) {
+        tiles.forEach(function(tilePoint) {
             var originalTilePoint = tilePoint.clone();
 
             if (layer._adjustTilePoint) {
                 layer._adjustTilePoint(tilePoint);
             }
-
-            var tilePos = originalTilePoint
-                .scaleBy(new L.Point(tileSize, tileSize))
-                .subtract(bounds.min);
+            
+            var tilePos = layer._getTilePos(originalTilePoint)
+                .subtract(bounds.min)
+                .add(origin);
 
             if (tilePoint.y >= 0) {
-                if (isCanvasLayer) {
-                    var tile = layer._tiles[tilePoint.x + ':' + tilePoint.y];
-                    tileQueue.defer(canvasTile, tile, tilePos, tileSize);
-                } else {
-                    var url = addCacheString(layer.getTileUrl(tilePoint));
-                    tileQueue.defer(loadTile, url, tilePos, tileSize);
-                }
+                var url = addCacheString(layer.getTileUrl(tilePoint));
+                tileQueue.defer(loadTile, url, tilePos, tileSize);
             }
         });
 
         tileQueue.awaitAll(tileQueueFinish);
 
-        function canvasTile(tile, tilePos, tileSize, callback) {
-            callback(null, {
-                img: tile,
-                pos: tilePos,
-                size: tileSize
-            });
-        }
-
         function loadTile(url, tilePos, tileSize, callback) {
             var im = new Image();
             im.crossOrigin = '';
-            im.onload = function () {
+            im.onload = function() {
                 callback(null, {
                     img: this,
                     pos: tilePos,
                     size: tileSize
                 });
-            };
-            im.onerror = function (e) {
-                // use canvas instead of errorTileUrl if errorTileUrl get 404
-                if (layer.options.errorTileUrl != '' && e.target.errorCheck === undefined) {
-                    e.target.errorCheck = true;
-                    e.target.src = layer.options.errorTileUrl;
-                } else {
-                    callback(null, {
-                        img: dummycanvas,
-                        pos: tilePos,
-                        size: tileSize
-                    });
-                }
             };
             im.src = url;
         }
@@ -185,14 +132,10 @@ module.exports = function leafletImage(map, callback) {
         canvas.height = dimensions.y;
         var ctx = canvas.getContext('2d');
         var pos = L.DomUtil.getPosition(root).subtract(bounds.min).add(origin);
-        try {
-            ctx.drawImage(root, pos.x, pos.y, canvas.width - (pos.x * 2), canvas.height - (pos.y * 2));
-            callback(null, {
-                canvas: canvas
-            });
-        } catch(e) {
-            console.error('Element could not be drawn on canvas', root); // eslint-disable-line no-console
-        }
+        ctx.drawImage(root, pos.x, pos.y);
+        callback(null, {
+            canvas: canvas
+        });
     }
 
     function handleMarkerLayer(marker, callback) {
@@ -201,24 +144,20 @@ module.exports = function leafletImage(map, callback) {
             pixelBounds = map.getPixelBounds(),
             minPoint = new L.Point(pixelBounds.min.x, pixelBounds.min.y),
             pixelPoint = map.project(marker.getLatLng()),
-            isBase64 = /^data\:/.test(marker._icon.src),
-            url = isBase64 ? marker._icon.src : addCacheString(marker._icon.src),
+            url = addCacheString(marker._icon.src),
             im = new Image(),
             options = marker.options.icon.options,
             size = options.iconSize,
             pos = pixelPoint.subtract(minPoint),
-            anchor = L.point(options.iconAnchor || size && size.divideBy(2, true));
-
-        if (size instanceof L.Point) size = [size.x, size.y];
-
-        var x = Math.round(pos.x - size[0] + anchor.x),
-            y = Math.round(pos.y - anchor.y);
+            anchor = L.point(options.iconAnchor || size && size.divideBy(2, true)),
+            x = pos.x - size[0] + anchor.x,
+            y = pos.y - anchor.y;
 
         canvas.width = dimensions.x;
         canvas.height = dimensions.y;
         im.crossOrigin = '';
 
-        im.onload = function () {
+        im.onload = function() {
             ctx.drawImage(this, x, y, size[0], size[1]);
             callback(null, {
                 canvas: canvas
@@ -226,163 +165,94 @@ module.exports = function leafletImage(map, callback) {
         };
 
         im.src = url;
-
-        if (isBase64) im.onload();
-    }
-    
-    function handleEsriDymamicLayer(dynamicLayer, callback) {
-        var canvas = document.createElement('canvas');
-        canvas.width = dimensions.x;
-        canvas.height = dimensions.y;
-    
-        var ctx = canvas.getContext('2d');
-    
-        var im = new Image();
-        im.crossOrigin = '';
-        im.src = addCacheString(dynamicLayer._currentImage._image.src);
-    
-        im.onload = function() {
-            ctx.drawImage(im, 0, 0);
-            callback(null, {
-                canvas: canvas
-            });
-        };
     }
 
     function addCacheString(url) {
-        // If it's a data URL we don't want to touch this.
-        if (isDataURL(url) || url.indexOf('mapbox.com/styles/v1') !== -1) {
-            return url;
-        }
-        return url + ((url.match(/\?/)) ? '&' : '?') + 'cache=' + cacheBusterDate;
+        return url + ((url.match(/\?/)) ? '&' : '?') + 'cache=' + (+new Date());
     }
-
-    function isDataURL(url) {
-        var dataURLRegex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i;
-        return !!url.match(dataURLRegex);
-    }
-
 };
 
-},{"d3-queue":2}],2:[function(require,module,exports){
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.d3_queue = global.d3_queue || {})));
-}(this, function (exports) { 'use strict';
-
-  var version = "2.0.3";
+},{"./queue":2}],2:[function(_dereq_,module,exports){
+(function() {
+  if (typeof module === "undefined") self.queue = queue;
+  else module.exports = queue;
+  queue.version = "1.0.4";
 
   var slice = [].slice;
 
-  var noabort = {};
+  function queue(parallelism) {
+    var q,
+        tasks = [],
+        started = 0, // number of tasks that have been started (and perhaps finished)
+        active = 0, // number of tasks currently being executed (started but not finished)
+        remaining = 0, // number of tasks not yet finished
+        popping, // inside a synchronous task callback?
+        error = null,
+        await = noop,
+        all;
 
-  function Queue(size) {
-    if (!(size >= 1)) throw new Error;
-    this._size = size;
-    this._call =
-    this._error = null;
-    this._tasks = [];
-    this._data = [];
-    this._waiting =
-    this._active =
-    this._ended =
-    this._start = 0; // inside a synchronous task callback?
-  }
+    if (!parallelism) parallelism = Infinity;
 
-  Queue.prototype = queue.prototype = {
-    constructor: Queue,
-    defer: function(callback) {
-      if (typeof callback !== "function" || this._call) throw new Error;
-      if (this._error != null) return this;
-      var t = slice.call(arguments, 1);
-      t.push(callback);
-      ++this._waiting, this._tasks.push(t);
-      poke(this);
-      return this;
-    },
-    abort: function() {
-      if (this._error == null) abort(this, new Error("abort"));
-      return this;
-    },
-    await: function(callback) {
-      if (typeof callback !== "function" || this._call) throw new Error;
-      this._call = function(error, results) { callback.apply(null, [error].concat(results)); };
-      maybeNotify(this);
-      return this;
-    },
-    awaitAll: function(callback) {
-      if (typeof callback !== "function" || this._call) throw new Error;
-      this._call = callback;
-      maybeNotify(this);
-      return this;
+    function pop() {
+      while (popping = started < tasks.length && active < parallelism) {
+        var i = started++,
+            t = tasks[i],
+            a = slice.call(t, 1);
+        a.push(callback(i));
+        ++active;
+        t[0].apply(null, a);
+      }
     }
-  };
 
-  function poke(q) {
-    if (!q._start) try { start(q); } // let the current task complete
-    catch (e) { if (q._tasks[q._ended + q._active - 1]) abort(q, e); } // task errored synchronously
-  }
-
-  function start(q) {
-    while (q._start = q._waiting && q._active < q._size) {
-      var i = q._ended + q._active,
-          t = q._tasks[i],
-          j = t.length - 1,
-          c = t[j];
-      t[j] = end(q, i);
-      --q._waiting, ++q._active;
-      t = c.apply(null, t);
-      if (!q._tasks[i]) continue; // task finished synchronously
-      q._tasks[i] = t || noabort;
+    function callback(i) {
+      return function(e, r) {
+        --active;
+        if (error != null) return;
+        if (e != null) {
+          error = e; // ignore new tasks and squelch active callbacks
+          started = remaining = NaN; // stop queued tasks from starting
+          notify();
+        } else {
+          tasks[i] = r;
+          if (--remaining) popping || pop();
+          else notify();
+        }
+      };
     }
-  }
 
-  function end(q, i) {
-    return function(e, r) {
-      if (!q._tasks[i]) return; // ignore multiple callbacks
-      --q._active, ++q._ended;
-      q._tasks[i] = null;
-      if (q._error != null) return; // ignore secondary errors
-      if (e != null) {
-        abort(q, e);
-      } else {
-        q._data[i] = r;
-        if (q._waiting) poke(q);
-        else maybeNotify(q);
+    function notify() {
+      if (error != null) await(error);
+      else if (all) await(error, tasks);
+      else await.apply(null, [error].concat(tasks));
+    }
+
+    return q = {
+      defer: function() {
+        if (!error) {
+          tasks.push(arguments);
+          ++remaining;
+          pop();
+        }
+        return q;
+      },
+      await: function(f) {
+        await = f;
+        all = false;
+        if (!remaining) notify();
+        return q;
+      },
+      awaitAll: function(f) {
+        await = f;
+        all = true;
+        if (!remaining) notify();
+        return q;
       }
     };
   }
 
-  function abort(q, e) {
-    var i = q._tasks.length, t;
-    q._error = e; // ignore active callbacks
-    q._data = undefined; // allow gc
-    q._waiting = NaN; // prevent starting
+  function noop() {}
+})();
 
-    while (--i >= 0) {
-      if (t = q._tasks[i]) {
-        q._tasks[i] = null;
-        if (t.abort) try { t.abort(); }
-        catch (e) { /* ignore */ }
-      }
-    }
-
-    q._active = NaN; // allow notification
-    maybeNotify(q);
-  }
-
-  function maybeNotify(q) {
-    if (!q._active && q._call) q._call(q._error, q._data);
-  }
-
-  function queue(concurrency) {
-    return new Queue(arguments.length ? +concurrency : Infinity);
-  }
-
-  exports.version = version;
-  exports.queue = queue;
-
-}));
-},{}]},{},[1])(1)
+},{}]},{},[1])
+(1)
 });
